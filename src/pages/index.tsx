ff6b96d8ff3ffc8eb2b2b11cf3926d77ef5105b8
Filipe Fernandes da Motta <filipe.motta@fga.com.br>
2021-04-08 16:39:03 -0300

    Projeto do Move.it com Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from "../components/ExperienceBar"
 import { Profile } from '../components/Profile'
 import { Countdown } from '../components/Countdown'
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+      level: number;
+      currentExperience: number;
+      challengesCompleted: number; 
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider 
+    level={props.level}
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}>
     <div className={styles.container}>
       <Head>
         <title>Início | Move.it</title>
@@ -15,6 +30,7 @@ export default function Home() {
 
       <ExperienceBar />
 
+      <CountdownProvider>
       <section>
         <div>
           <Profile />
@@ -22,10 +38,27 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
+      </CountdownProvider>
     </div>
+    </ChallengesProvider>
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
+
+// Back-end (Ruby)
+// Next.js (Node.js)
+// Front-end (React)
